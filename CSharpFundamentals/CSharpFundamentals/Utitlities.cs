a5f4866ce0f370c63118b34f8fbbf84e57b7bc66
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2020-02-02 11:36:22 +0000

    Add Utilities: generate random passwd

--- CSharpFundamentals/CSharpFundamentals/Utitlities.cs
+++ CSharpFundamentals/CSharpFundamentals/Utitlities.cs
@@ -18,5 +18,19 @@ namespace CSharpFundamentals
                 Console.WriteLine(i);
             }
         }
+        public static void generatePassword()
+        {
+          const int passwordLength = 10;
+
+          var buffer = new char[passwordLength];
+          for (var i=0; i<passwordLength; i++)
+          {
+            var random = new Random();
+            // Console.WriteLine((char)('a' + random.Next(1, 10)));
+            buffer[i] = (char)('a' + random.Next(0, 26));
+           
+          }
+          Console.WriteLine(string.Format("Generated password: {0}\n", new string(buffer)));
+        }
     }
 }
